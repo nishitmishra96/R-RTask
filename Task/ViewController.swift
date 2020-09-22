56adbc0cd79360a0973d3fc012eca980b1c10793
Nishit Mishra <nishit@wynk.in>
2020-09-23 00:16:32 +0530

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Vikas on 23/09/20.
+//  Copyright © 2020 XYZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
